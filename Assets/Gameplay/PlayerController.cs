330e037ca3ce0a646eb0ae1c26336c426a987114
Zack Turman <zkturman@gmail.com>
2022-09-18 21:44:15 +0100

    added basic player input

--- Assets/Gameplay/PlayerController.cs
+++ Assets/Gameplay/PlayerController.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour, PaddleController
 {
-    // Start is called before the first frame update
-    void Start()
+    private KeyCode[] RelativeUpKeys = {KeyCode.W, KeyCode.UpArrow};
+    private KeyCode[] RelativeDownKeys = {KeyCode.S, KeyCode.DownArrow};
+    private KeyCode[] PauseKeys = { KeyCode.Escape };
+    public PlayerBehaviour PlayerBehaviour { get; set; }
+
+    private void Awake()
     {
-        
+        PlayerBehaviour = GetComponent<PlayerBehaviour>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (IsRelativeUpInputPressed())
+        {
+            HandleRelativeUpInput();
+        }
+        else if (IsRelativeDownInputPressed())
+        {
+            HandleRelativeDownInput();
+        }
+        else if (IsPauseInput())
+        {
+            HandlePauseInput();
+        }
+    }
+
+    public bool IsRelativeUpInputPressed()
+    {
+        return isPressedKeyInSet(RelativeUpKeys);
+    }
+
+    public bool IsRelativeDownInputPressed()
+    {
+        return isPressedKeyInSet(RelativeDownKeys);
+    }
+
+    public bool IsPauseInput()
+    {
+        return isPressedKeyInSet(PauseKeys);
+    }
+
+    private bool isPressedKeyInSet(KeyCode[] validKeys)
+    {
+        bool isValidKey = false;
+        for (int i = 0; i < validKeys.Length; i++)
+        {
+            if (Input.GetKey(validKeys[i]))
+            {
+                isValidKey = true;
+            }
+        }
+        return isValidKey;
+    }
+
+    public void HandleRelativeUpInput()
+    {
+        PlayerBehaviour.MovePaddleRelativelyUp();
+    }
+
+    public void HandleRelativeDownInput()
+    {
+        PlayerBehaviour.MovePaddleRelativelyDown();
+    }
+
+    public void HandlePauseInput()
+    {
+
     }
 }
